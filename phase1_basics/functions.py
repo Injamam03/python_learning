7ee7a45e2ff4d0a412777c509c0d2eb2d828e417
injam <injamamulh544@gmail.com>
2026-06-02 09:25:33 +0600

    add two number function

---
+++ phase1_basics/functions.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the secound nmber: "))
+add = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+
+
+print("This the add number ",add)
+print("This the sub number ",sub)
+print("This the mul number ",mul)
+print("This the div number ",div)
