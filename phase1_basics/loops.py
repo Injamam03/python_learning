f24aafc21b0c9d85d62af93548587218ba022887
injam <injamamulh544@gmail.com>
2026-06-02 11:41:45 +0600

    loop file update

--- phase1_basics/loops.py
+++ phase1_basics/loops.py
@@ -14,3 +14,20 @@ print(" ================  this  3rd for loop =======================")
 for i in range (-8,0): 
     print(i)
 
+print(" ================  this  4th for loop =======================")
+
+
+frutisList = ["Apple", "Banana", "Mango", "Orange","lemon"]
+for frut in frutisList:
+  print(frut)
+
+print(" ================  this  5th for loop =======================")
+
+for index, frut in enumerate(frutisList):
+    print(f"{index} = ",frut)
+ 
+print(" ================  this  6th for loop =======================")
+name = "Injamam Ul Hoque Sifat"
+
+for l in name:
+    print(l)
