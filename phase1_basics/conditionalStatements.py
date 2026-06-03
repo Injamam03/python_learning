e4ab38196e6ec23af7b92a5209ca6f47dd80d358
injam <injamamulh544@gmail.com>
2026-06-03 10:40:02 +0600

    update conditionalStatement file

--- phase1_basics/conditionalStatements.py
+++ phase1_basics/conditionalStatements.py
@@ -1,4 +1,7 @@
 
+
+print(" ================  00000000 =======================")
+
 # age= int(input("Enter Your Age : "))
 
 # if(age>=18):
@@ -6,14 +9,43 @@
 # else:
 #     print(" your are invaild for vote")
 
-light = input("light : ")
+print(" ================  00000000 =======================")
 
-if (light == "red"):
-    print("don't go")
+# light = input("light Color : ")
 
-elif(light == "yellow"):
-    print("wait")
-elif(light=="green"):
-    print("go")
+# if (light == "red"):
+#     print("Stop and wait for green")
+
+# elif(light == "yellow"):
+#     print("wait a moment")
+# elif(light=="green"):
+#     print("go")
+# else:
+#     print("light is broken")
+
+
+
+
+mark = int(input("Enter your marks : "))
+if (mark >=90):
+    print("Your got golden A+")
+
+elif(mark >=80 and mark<90):
+    print(" your got A+")
+
+elif(mark >=70 and mark<80):
+    print(" your got A")
+
+elif(mark >=60 and mark<70):
+    print(" your got B+")
+
+elif(mark >=50 and mark<60):
+    print(" your got B")
+
+elif(mark >=40 and mark<50):
+    print(" your got C")
+    
+elif(mark >=33 and mark<40):
+    print(" your got D")
 else:
-    print("light is broken")
+    print(" you are failed")
