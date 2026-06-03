8a77c41307bce0837b20f8709e2b15c159105d32
injam <injamamulh544@gmail.com>
2026-06-03 09:27:40 +0600

    update function file

--- phase1_basics/forLoops.py
+++ phase1_basics/forLoops.py
@@ -1,45 +1,71 @@
-print(" ================  this 1st for loop =======================")
+# print(" ================  this 1st for loop =======================")
 
-for i in range (5):
-    print(i)
+# for i in range (5):
+#     print(i)
 
-print(" ================  this 2nd for loop =======================")
-
-
-for i in range (5,20):
-    print(i)
-
-print(" ================  this  3rd for loop =======================")
-
-for i in range (-8,0): 
-    print(i)
-
-print(" ================  this  4th for loop =======================")
+# print(" ================  this 2nd for loop =======================")
 
 
-frutisList = ["Apple", "Banana", "Mango", "Orange","lemon"]
-for frut in frutisList:
-  print(frut)
+# for i in range (5,20):
+#     print(i)
 
-print(" ================  this  5th for loop =======================")
+# print(" ================  this  3rd for loop =======================")
 
-for index, frut in enumerate(frutisList):
-    print(f"{index} = ",frut)
+# for i in range (-8,0): 
+#     print(i)
+
+# print(" ================  this  4th for loop =======================")
+
+
+# frutisList = ["Apple", "Banana", "Mango", "Orange","lemon"]
+# for frut in frutisList:
+#   print(frut)
+
+# print(" ================  this  5th for loop =======================")
+
+# for index, frut in enumerate(frutisList):
+#     print(f"{index} = ",frut)
  
-print(" ================  this  6th for loop =======================")
-name = "Injamam Ul Hoque Sifat"
+# print(" ================  this  6th for loop =======================")
+# name = "Injamam Ul Hoque Sifat"
 
-for letter in name:
-    print(letter)
+# for letter in name:
+#     print(letter)
 
-print(" ================  this  7th for loop =======================")
-
-
-for i in range(1,6):
-    print(f"5 X {i} = {5*i} ")
+# print(" ================  this  7th for loop =======================")
 
 
-print(" ================  this  7th for loop =======================")
+# for i in range(1,6):
+#     print(f"5 X {i} = {5*i} ")
 
-for i in range(1,10):
-    print(i," Injamam")
+
+# print(" ================  this  8th for loop =======================")
+
+# for i in range(1,10):
+#     print(i," Injamam")
+    
+
+
+# a, b = 2,5
+# tx = "*"
+# print(2*tx*5)
+
+
+# a, b = "2",5
+# tx = "*"
+# print((a+tx)*b)
+
+
+# old=14;
+# print(type(old))
+
+# a, b, c = 5,6,6;
+# d = 20;
+# print(a+b+c*d)
+
+
+# a , b  = 20, 6
+# c = a/b
+# d = a%b
+# print(c,)
+# print(d)
--- phase1_basics/functions.py
+++ phase1_basics/functions.py
@@ -11,3 +11,17 @@ print("This the add number ",add)
 print("This the sub number ",sub)
 print("This the mul number ",mul)
 print("This the div number ",div)
+
+
+
+
+
+
+print(" ================  00000000 =======================")
+
+
+name = str(input("Enter Your Name : "))
+age = int(input("Enter your age : "))
+homeAdress = str(input("Enter Your Home Adress : "))
+
+print("MY name is", name ,". I am ", age , " and I am from", homeAdress)
