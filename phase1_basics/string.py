6dcb2263c0935f5627af8c61b5f323589911cb06
injam <injamamulh544@gmail.com>
2026-06-03 15:51:28 +0600

    add a new file

---
+++ phase1_basics/string.py
@@ -0,0 +1,19 @@
+str1 = "Injamam Ul Hoque Sifat"
+print(len(str1))
+
+print(str1)
+
+# python slicing indexing ==================
+print(str1[4])
+print(str1[:4])
+print(str1[4:])
+print(str1[1:4])
+print(str1[::])
+print(str1[1:])
+print(str1[:25])
+print("==================== 000000000000000000000 ================= ")
+print(str1[-4:])
+print(str1[-4:-1])
+print(str1[::])
+print(str1[-1:])
+print(str1[:-10])
