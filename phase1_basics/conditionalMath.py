973f69e9b49919623936f8ccb3d976d76ff48839
injam <injamamulh544@gmail.com>
2026-06-04 08:55:23 +0600

    add a new file

---
+++ phase1_basics/conditionalMath.py
@@ -0,0 +1,59 @@
+"""
+print(" ================  00000000 =======================")
+# check the number even number or odd number
+
+number =45
+
+result = number%2
+
+if(result == 0):
+    print("This is even number :",number)
+else:
+    print("This is odd number :",number)
+
+print(" ================  00000000 =======================")
+
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+c = int(input("Enter the third number : "))
+if ( (a>b) and (a>c)):
+    print("first number : ", a)
+elif((b>a) and (b>c)):
+    print("second number : ",b)
+else:
+    print("third number : ", c)
+
+print(" ================  00000000 =======================")
+
+num = 46
+if (num%5 ==0):
+    print("This is multiple number :", num)
+else:
+    print("This is not multiple number :",num)
+
+"""
+
+student = ["Inajamam","Shawon","Sagar","Rasel"]
+student[0]= "Sifat"
+print(student)
+print(student[3])
+print(student[:3])
+print(student[0:])
+
+print(" ================  00000000 =======================")
+
+student = ["Inajamam",25,90,"Rasel"]
+print(student[1:3])
+
+print(" ================  00000000 =======================")
+student = [40,25,90,50]
+student.append(60)
+print(student)
+print(student.sort(reverse=True))
+print(student)
+
+print(" ================  00000000 =======================")
+
+list = [40,25,90,50]
+list.reverse()
+print(list)
