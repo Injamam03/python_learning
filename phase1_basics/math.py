4c40ebc0ed89fd8d83381578e0aadb206e228174
injam <injamamulh544@gmail.com>
2026-06-15 10:46:31 +0600

    update

--- phase1_basics/math.py
+++ phase1_basics/math.py
@@ -1,6 +1,18 @@
-def multiply(numbers):
-    total = 1
-    for x in numbers: 
-        total *= x 
-    return total
-print(multiply([8,6,9,10,7]))
+#  ============== page 32 and program No : 03 ================
+
+# def multiply(numbers):
+#     total = 1
+#     for x in numbers: 
+#         total *= x 
+#     return total
+# print(" this is my total muliplication answer: ",multiply([8,6,9,10,7]))
+
+#  =============== 00000000000000000000000 ========================== 
+
+def factorial(n):
+    if n == 0 :
+        return 1
+    else:
+        return n*factorial(n-1)
+n = int(input("Input a number : "))
+print(factorial(n))
