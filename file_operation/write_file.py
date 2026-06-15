d02e4d40c76c301a9f2e394bc1ec11a8c197ba42
injam <injamamulh544@gmail.com>
2026-06-15 16:37:50 +0600

    update file operation file

--- file_operation/write_file.py
+++ file_operation/write_file.py
@@ -9,3 +9,62 @@ with open("file_operation/data.txt", "w", encoding="utf-8") as file:
     file.write("আমি Python শিখছি\n")
     file.write("আমি Python শিখছি\n")
     
+
+with open("file_operation/marks.txt", "w", encoding="utf-8") as file:
+    file.write("Injamam = 95\n")
+    file.write("Karim = 88\n")
+
+with open("file_operation/marks.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        if "Karim" in line:
+            print(line)
+
+
+
+with open("file_operation/data.txt", "a", encoding="utf-8") as file:
+    file.write("আমি Flutter developer\n")
+    file.write("আমি Dhaka তে থাকি\n")
+
+
+
+
+
+with open("file_operation/sifat.txt", "w", encoding="utf-8") as file:
+    file.write("sifat = 95\n")
+    file.write("sagar = 88\n")
+
+import os
+os.remove("file_operation/sifat.txt")
+print("File Deleted")
+
+
+
+
+
+# =========================================
+#  close() — File বন্ধ করা 
+file = open("file_operation/data.txt", "r", encoding="utf-8")
+content = file.read()
+print(content)
+file.close()  #  শেষে বন্ধ করো
+
+# readline() — এক লাইন পড়া
+with open("file_operation/data.txt", "r", encoding="utf-8") as file:
+    line1 = file.readline()  # শুধু প্রথম লাইন
+    line2 = file.readline()  # শুধু দ্বিতীয় লাইন
+    print(line1)
+    print(line2)
+
+
+
+#  readlines() — সব লাইন list এ 
+with open("file_operation/data.txt", "r", encoding="utf-8") as file:
+    lines = file.readlines()  # সব লাইন list এ আসবে
+    print(lines)
+
+#  readable() — পড়া যাবে কিনা check
+with open("file_operation/data.txt", "r", encoding="utf-8") as file:
+    print(file.readable())  # True
+
+with open("file_operation/data.txt", "w", encoding="utf-8") as file:
+    print(file.readable())  # False (write mode এ পড়া যায় না)
