41b9474c4b5793e3302065b7d10cb43669d6aae0
injam <injamamulh544@gmail.com>
2026-06-15 11:05:33 +0600

    add Program to determine if a struggle is fundamental using functions

--- phase1_basics/math.py
+++ phase1_basics/math.py
@@ -1,5 +1,5 @@
 #  ============== page 32 and program No : 03 ================
-
+#Program to calculate the product of numbers using the function:
 # def multiply(numbers):
 #     total = 1
 #     for x in numbers: 
@@ -9,10 +9,36 @@
 
 #  =============== 00000000000000000000000 ========================== 
 
-def factorial(n):
-    if n == 0 :
-        return 1
+#  ============== page 32 and program No : 04 ================
+
+# Program to find the factorial value of a number using the function:
+
+
+# def factorial(n):
+#     if n == 0 :
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# n = int(input("Input a number : "))
+# print(factorial(n))
+
+
+
+#  =============== 00000000000000000000000 ========================== 
+
+#  ============== page 32 and program No : 05 ================
+
+# Program to determine if a struggle is fundamental using functions
+
+def test_prime(n):
+    if (n==1):
+        return False
+    elif(n==2):
+        return True
     else:
-        return n*factorial(n-1)
-n = int(input("Input a number : "))
-print(factorial(n))
+        for x in range(2,n):
+            if(n%x==0):
+                return False
+        return True
+print(test_prime(int(input("Input Number: "))))
+
