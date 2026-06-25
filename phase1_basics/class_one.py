1c58c42910f36a041840f18ad30906cbf6c77443
injam <injamamulh544@gmail.com>
2026-06-25 16:45:58 +0600

    update

--- phase1_basics/class_one.py
+++ phase1_basics/class_one.py
@@ -1,34 +1,25 @@
-print("==============================")
-print("==============================")
+print("====================")
+def multi(numbers):
+    total = 1
+    for x in numbers:
+        total = total * x
+    return total      
 
-"""a = int(input("Enter the value of a : "))
-b = int(input("Enter the value of b :"))
-def add_fun(a,b):
-    c =a+b
-    return c
-def sub_fun(a,b):
-    c =a-b
-    return c
-def mul_fun(a,b):
-    c =a*b
-    return c
-def div_fun(a,b):
-    c =a/b
-    return c
-print("add a & b is :",add_fun(a,b))
-print("sub a & b is :",sub_fun(a,b))
-print("mul a & b is :",mul_fun(a,b))
-print("div a & b is :",div_fun(a,b))"""
+print(multi((8, 5, 6, 7))) 
 
 
-"""def calculator(a,b):
-    print('Addition      =',a+b)
-    print('Subtraction   =',a-b)
-    print('Multiplication =',a*b)
-    print('Division      =',a/b)
 
-calculator(50,5)"""
+
+
+
+
+
+
+
+
+"""for i in range(1,50):
+    print(f"5 x {i} =",i*5)"""
  
-    
+
 
 
