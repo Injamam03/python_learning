f4dcbe3169af2a936ca4c9d8c78205eadfc27b7b
injam <injamamulh544@gmail.com>
2026-07-06 10:56:05 +0600

    update

--- phase2_oop/inheritance_practice.py
+++ phase2_oop/inheritance_practice.py
@@ -1,23 +1,19 @@
-class Calculator:
-    """Super Class to define addition, subtraction, multiplication and division."""
+"""class Calculator:
+    Super Class to define addition, subtraction, multiplication and division.
     def addition(self, x, y):
         return x + y
-
     def subtraction(self, x, y):
         return x - y
-
     def multiplication(self, x, y):
         return x * y
-
     def division(self, x, y):
         try:
             return x / y
         except ZeroDivisionError:
             return 'It is impossible to divide by zero.'
 
-
 class SubCalculator(Calculator):
-    """Child class define. To calculate square and cube."""
+    Child class define. To calculate square and cube.
     def square(self, x):
         return x * x
 
@@ -27,7 +23,6 @@ class SubCalculator(Calculator):
 
 my_calculator = SubCalculator()
 temp = my_calculator.addition(60, 30)
-
 print("X+Y:", temp)
 temp = my_calculator.subtraction(60, 30)
 print("X-Y:", temp)
@@ -38,4 +33,46 @@ print("X/Y:", temp)
 temp = my_calculator.square(9)
 print("Square of 9:", temp)
 temp = my_calculator.cube(5)
-print("cube of 5:", temp)
+print("cube of 5:", temp)"""
+
+
+class student:
+     def __init__(self,name , roll, mark):
+          self.name = name
+          self.roll = roll
+          self.mark = mark 
+
+     def show(self):
+        print("Name"+ self.name)
+        # print(f"Name = {self.name}")
+        print(f"Roll = {self.roll}")
+        print(f"Mark = {self.mark}")
+        print("-------------------")
+
+
+    
+s1 = student("Injamam",20,50)
+s2 = student("Shawon",10,40)
+s3 = student("monir",60,30)
+
+s1.show()
+s2.show()
+s3.show()
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
